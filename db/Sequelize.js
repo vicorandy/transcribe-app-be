3f1a9754e83ca0e41144ff4e15a86e3fefb27a43
vicorandy <vicorandyrichard26@gmail.com>
2025-06-23 06:29:46 -0400

    database connecton render

--- db/Sequelize.js
+++ db/Sequelize.js
@@ -1,23 +1,28 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const { password, user, database ,host} = process.env;
-
-const db = new Sequelize(database, user, password, {
-  host,
+const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true, // for Render PostgreSQL
+      rejectUnauthorized: false,
+    },
+  },
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000,
   },
-  logging: false, // Disable logging
 });
 
 // testing db connection
 function dbConnectionTest() {
-  db.authenticate().then(() => console.log('database connected...'));
+  db.authenticate().then(() => console.log('database connected...'))
+    .catch(err => console.error('Unable to connect:', err));
 }
 
 module.exports = { db, dbConnectionTest };
